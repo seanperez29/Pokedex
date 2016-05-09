dac1f2fb9f3be7c1bcde245e8d7f4a213f212903
Sean Perez <seanperez@Seans-MBP-2.attlocal.net>
2016-05-08 17:51:01 -0700

    Changes up to search problem

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Sean Perez on 5/8/16.
+//  Copyright © 2016 Sean Perez. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon:Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
